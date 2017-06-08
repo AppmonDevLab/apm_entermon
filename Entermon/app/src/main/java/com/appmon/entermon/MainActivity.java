646749826432bd70449dc12f6006e659b065958c
SYSUGrain <guyi3@mail2.sysu.edu.cn>
2017-06-08 17:07:58 +0800

    Revert "v1.0"
    This reverts commit 7ac16a2e8e5372e52d40b4462c252f37a224037d.

--- Entermon/app/src/main/java/com/appmon/entermon/MainActivity.java
+++ Entermon/app/src/main/java/com/appmon/entermon/MainActivity.java
@@ -1,10 +1,7 @@
 package com.appmon.entermon;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,23 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bt_to_movie = (Button) findViewById(R.id.movie_bt);
-        bt_to_movie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(MainActivity.this, MovieActivity.class);
-                startActivity(intent);
-
-            }
-        });
-        Button bt_to_login = (Button) findViewById(R.id.login_bt);
-        bt_to_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
